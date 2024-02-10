7152f45f2c3959a65373ac9d2a59f31fd2b03131
funun <dx94ke.tmu@gmail.com>
2024-02-10 14:09:21 +0900

    メインファイル追加

--- guess_the_number_game.py
+++ guess_the_number_game.py
@@ -1,18 +1,65 @@
-import subprocess
+import random
 import sys
 
-"""
-さて、標準的なストリームを扱ったので、今度はこの知識を使ってプログラムを作ってみましょう。
-この課題では、ユーザーに 2 つの数字、最小数（n）と最大数（m）を入力してもらうことになります。
-最小値が最大値以下であることを確認することが重要です。
-ユーザーは、この 2 つの数字を入力すると、プログラムが n から m の範囲内で乱数を生成します。
-その後、ユーザーは乱数を正しく推測するまで、ゲームループの中で繰り返し数字を入力することに
-なります。与えられた範囲内の乱数を生成するには、random モジュールと randint 関数を
-使用してください。ゲームをより難しくするために、ユーザーが数字を推測するための試行回数を
-制限することができます。この場合、for 文で最大 n 回の試行を行うか、while 文でユーザーが
-数字を正しく当てるまで繰り返し入力する方法があります。
-"""
+class Decidenumber:
+    def __init__(self):
+        self.min = 0
+        self.max = 100
 
+    def inputMinMax(self):
+        while True:
+            try:
+                sys.stdout.buffer.write(b'Guess the number game. Enter minimam number:\n')
+                sys.stdout.flush()
+                self.min = int(sys.stdin.buffer.readline().strip())
+                sys.stdout.buffer.write(b'Enter maximum number:\n')
+                sys.stdout.flush() 
+                self.max = int(sys.stdin.buffer.readline().strip()) 
+                if self.min < self.max:
+                    break
+                else:
+                    sys.stdout.buffer.write(b"Make the value greater than the minimum value\n")
+            except ValueError:
+                sys.stdout.buffer.write(b"You can enter only number\n")
+        return random.randint(self.min,self.max)
+        
+class Gameplay:
+    def __init__(self):
+        self.game_number = Decidenumber().inputMinMax()
 
+    def userInput(self):
+        while True:
+            sys.stdout.buffer.write(b'Enter your choice:\n')
+            sys.stdout.flush()
+            guess = sys.stdin.buffer.readline().strip() 
+            try:
+                guess = int(guess)
+                return guess
+            except ValueError:
+                sys.stdout.buffer.write(b"You can enter only number\n")
+        
+    def outputMessage(self,messege):
+        sys.stdout.buffer.write(messege)
+        
+    def judgeTheGame(self,guess):
+        if self.game_number == guess:
+            return True
+        elif guess < self.game_number:
+            self.outputMessage(b"Guess a bigger number\n")
+            return False
+        else:
+            self.outputMessage(b"Guess a smaller number\n")
+            return False
 
+    def play(self):
+        for i in range(7):
+            guess = self.userInput()
+            if self.judgeTheGame(guess):
+                sys.stdout.buffer.write(b"You win the game!\n")
+                break
+        else:
+            lose_message = f"You lose. The number is {self.game_number}.\n".encode("utf-8")
+            sys.stdout.buffer.write(lose_message)
 
+if __name__ == "__main__":
+    Gameplay().play()
